b07f06ba8450accb8cc11f0823f7a2d7e5770a73
brett <brett@brettfischl.com>
2021-11-04 19:00:44 -0400

    add ssl package

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule Webhooks.MixProject do
   def application do
     [
       mod: {Webhooks.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :ssl]
     ]
   end
 
